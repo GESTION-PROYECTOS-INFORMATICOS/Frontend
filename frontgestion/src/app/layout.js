0ace15a6d295f2b5f436b29eec9b173c603f9662
bastian176x <bastian.freire@alumnos.uv.cl>
2025-07-18 20:41:35 -0400

    Documento descargable y autocompletado de nombre

--- frontgestion/src/app/layout.js
+++ frontgestion/src/app/layout.js
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import { usePathname } from "next/navigation";
 import { signOut } from "next-auth/react";
 import AppBar from "@mui/material/AppBar";
@@ -6,33 +6,35 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import { Providers } from "./providers"; // importa el SessionProvider
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-
   const showAppBar = pathname !== "/login";
 
   return (
     <html lang="es">
       <body>
-        {showAppBar && (
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Mi Aplicación
-              </Typography>
-              <Button color="inherit" onClick={() => window.location.href = "/inicio"}>
-                Inicio
-              </Button>
-              <Button color="inherit" onClick={() => signOut({ callbackUrl: "/login" })}>
-                Cerrar sesión
-              </Button>
-            </Toolbar>
-          </AppBar>
-        )}
-        <Box sx={{ padding: 2 }}>
-          {children}
-        </Box>
+        <Providers> {/* <--- Aquí envolvemos toda la app */}
+          {showAppBar && (
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  Mi Aplicación
+                </Typography>
+                <Button color="inherit" onClick={() => window.location.href = "/inicio"}>
+                  Inicio
+                </Button>
+                <Button color="inherit" onClick={() => signOut({ callbackUrl: "/login" })}>
+                  Cerrar sesión
+                </Button>
+              </Toolbar>
+            </AppBar>
+          )}
+          <Box sx={{ padding: 2 }}>
+            {children}
+          </Box>
+        </Providers>
       </body>
     </html>
   );
